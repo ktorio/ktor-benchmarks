131cad9444f62540644ff5695f201f4905d6c3b9
Leonid Stashevsky <e5l@users.noreply.github.com>
2021-04-02 09:42:36 +0300

    KTOR-1723 Add legacy Ktor benchmarks (#2)

---
+++ benchmark-server/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+val ktor_version: String by project.extra
+
+group = "io.ktor"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("io.ktor:ktor-server-cio:$ktor_version")
+    implementation("io.ktor:ktor-websockets:$ktor_version")
+}
---
+++ server-benchmarks/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.32"
+    id("me.champeau.gradle.jmh") version "0.5.3"
+}
+
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+val okhttpVersion = "4.9.1"
+val apacheHttpClientVersion = "4.5.13"
+val logbackVersion = "1.2.3"
+val ktor_version = "1.5.2"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    jmh(kotlin("stdlib"))
+    jmh("io.ktor:ktor-server-cio:$ktor_version")
+    jmh("io.ktor:ktor-server-jetty:$ktor_version")
+    jmh("io.ktor:ktor-server-netty:$ktor_version")
+    jmh("io.ktor:ktor-client-cio:$ktor_version")
+    jmh("io.ktor:ktor-server-test-host:$ktor_version")
+
+    jmh("org.apache.httpcomponents:httpclient:$apacheHttpClientVersion")
+    jmh("com.squareup.okhttp3:okhttp:$okhttpVersion")
+    jmh("ch.qos.logback:logback-classic:$logbackVersion")
+}
