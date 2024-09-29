2c2f290df76007778e70b9cc5373f6e49339eda3
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-09-29 12:31:22 +0200

    Migrate server-benchmarks to version catalogs

--- server-benchmarks/build.gradle.kts
+++ server-benchmarks/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.allopen") version "2.0.20"
-    id("me.champeau.jmh") version "0.7.2"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.allopen)
+    alias(libs.plugins.jmh)
 }
 
 allOpen {
@@ -11,26 +11,17 @@ allOpen {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-val okhttpVersion = "4.12.0"
-val apacheHttpClientVersion = "4.5.14"
-val logbackVersion = "1.5.7"
-val ktor_version = "3.0.0-rc-1"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     jmh(kotlin("stdlib"))
-    jmh("io.ktor:ktor-server-core:$ktor_version")
-    jmh("io.ktor:ktor-server-cio:$ktor_version")
-    jmh("io.ktor:ktor-server-jetty:$ktor_version")
-    jmh("io.ktor:ktor-server-netty:$ktor_version")
-    jmh("io.ktor:ktor-client-core:$ktor_version")
-    jmh("io.ktor:ktor-client-cio:$ktor_version")
-    jmh("io.ktor:ktor-server-test-host:$ktor_version")
+    jmh(libs.ktor.server.core)
+    jmh(libs.ktor.server.cio)
+    jmh(libs.ktor.server.jetty.jakarta)
+    jmh(libs.ktor.server.netty)
+    jmh(libs.ktor.client.core)
+    jmh(libs.ktor.client.cio)
+    jmh(libs.ktor.server.test.host)
 
-    jmh("org.apache.httpcomponents:httpclient:$apacheHttpClientVersion")
-    jmh("com.squareup.okhttp3:okhttp:$okhttpVersion")
-    jmh("ch.qos.logback:logback-classic:$logbackVersion")
+    jmh(libs.apache.httpclient)
+    jmh(libs.okhttp)
+    jmh(libs.logback.classic)
 }
