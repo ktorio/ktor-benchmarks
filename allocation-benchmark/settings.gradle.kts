949122d46b61421e45e5b44b71cd579069b4d4cd
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-09-29 12:10:27 +0200

    Migrate allocation-benchmark to version catalogs

--- allocation-benchmark/settings.gradle.kts
+++ allocation-benchmark/settings.gradle.kts
@@ -1,5 +1,28 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         mavenCentral()
     }
 }
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap") {
+            content { includeGroupAndSubgroups("io.ktor") }
+        }
+        mavenCentral()
+        mavenLocal()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../libs.versions.toml"))
+
+            val ktorVersion: String? by settings.extra
+            ktorVersion?.let { version("ktor", it) }
+        }
+    }
+}
+
+rootProject.name = "allocation-benchmark"
