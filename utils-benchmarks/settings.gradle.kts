bb60ce9062e489f38f9a5286aef699ce18dfaf5e
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-09-29 12:37:49 +0200

    Migrate utils-benchmarks to version catalogs

--- utils-benchmarks/settings.gradle.kts
+++ utils-benchmarks/settings.gradle.kts
@@ -1,2 +1,17 @@
-rootProject.name = "utils-benchmarks"
+@file:Suppress("UnstableApiUsage")
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../libs.versions.toml"))
+
+            version("ktor", "2.1.0")
+        }
+    }
+}
+
+rootProject.name = "utils-benchmarks"
