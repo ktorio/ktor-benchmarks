bfafe3cc5932a2127acd78fe87940cd84005ba44
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-09-30 12:15:15 +0200

    Update benchmark-server to 3.0.0

--- benchmark-server/build.gradle.kts
+++ benchmark-server/build.gradle.kts
@@ -8,6 +8,7 @@ version = "1.0-SNAPSHOT"
 dependencies {
     implementation(kotlin("stdlib"))
 
+    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.cio)
-    implementation(libs.ktor.websockets)
+    implementation(libs.ktor.server.websockets)
 }
--- benchmark-server/src/main/kotlin/Utils.kt
+++ benchmark-server/src/main/kotlin/Utils.kt
@@ -1,7 +1,8 @@
 import io.ktor.utils.io.core.*
+import kotlinx.io.*
 
 fun makeArray(size: Int): ByteArray = buildPacket {
     repeat(size) {
         writeByte(it.toByte())
     }
-}.readBytes()
+}.readByteArray()
