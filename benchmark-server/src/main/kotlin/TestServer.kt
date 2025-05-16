01d90e3e2357aca3bd397f51f53740a52637671d
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-05-16 13:01:12 +0200

    Add a plugin to run benchmark-server before tests

--- benchmark-server/src/main/kotlin/TestServer.kt
+++ benchmark-server/src/main/kotlin/TestServer.kt
@@ -1,8 +1,17 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
-    embeddedServer(CIO, port=8080 ) {
-        benchmarks()
-    }.start(wait = true)
+    startServer(port = 8080, wait = true)
+}
+
+fun startServer(port: Int, wait: Boolean): EmbeddedServer<*, *> {
+    return embeddedServer(CIO, port, module = Application::benchmarks).apply {
+        start(wait)
+    }
 }
