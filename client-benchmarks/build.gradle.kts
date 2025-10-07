254bc1527439b94ba074fc46dfc75d716b289276
Leonid Stashevsky <e5l@users.noreply.github.com>
2025-10-07 10:26:00 +0200

    KTOR-1121 Add client benchmarks (#74)

--- client-benchmarks/build.gradle.kts
+++ client-benchmarks/build.gradle.kts
@@ -1,10 +1,24 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("io.ktor.benchmarks.ClientBenchmarkKt")
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.apache)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.java)
+    implementation(libs.okhttp)
+    implementation(libs.logback.classic)
 }
